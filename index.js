7bc5a63b20008043312bb04507fd8ef795401375
Mmackz <matthewmackenz@gmail.com>
2022-02-09 20:00:23 -0800

    ✨ add color to barchart

--- index.js
+++ index.js
@@ -14,18 +14,44 @@ const chartContainer = d3
 
 d3.json(
    "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-).then((data) => {
-   barWidth = width / data.data.length;
-   const gdpData = data.data.map((item) => item[1]);
-   console.log(gdpData);
+).then(({ data }) => {
+   barWidth = width / data.length;
+
+   const dates = data.map((item) => new Date(item[0]));
+   const maxDate = d3.max(dates);
+   const xAxisScale = d3
+      .scaleTime()
+      .domain([d3.min(dates), maxDate])
+      .range([0, width]);
+   const xAxis = d3.axisBottom(xAxisScale);
+   chartContainer
+      .append("g")
+      .call(xAxis)
+      .attr("id", "x-axis")
+      .attr("transform", "translate(60, 410)");
+
+   // GDP data (y-axis)
+   const gdpData = data.map((item) => item[1]);
    const maximum = d3.max(gdpData);
-   console.log(maximum);
    const scale = d3.scaleLinear().domain([0, maximum]).range([0, height]);
    const scaledData = gdpData.map((item) => scale(item));
-   console.log(scaledData);
    const yAxisScale = d3.scaleLinear().domain([0, maximum]).range([height, 0]);
    const yAxis = d3.axisLeft(yAxisScale);
+   chartContainer
+      .append("g")
+      .call(yAxis)
+      .attr("id", "y-axis")
+      .attr("transform", "translate(60, 10)");
 
-   chartContainer.append("g").call(yAxis).attr("id", "y-axis").attr("transform", "translate(60, 10)");
-   d3.select("svg").selectAll("rect").data(scaledData).enter().append("rect").attr
+   d3.select("svg")
+      .selectAll("rect")
+      .data(scaledData)
+      .enter()
+      .append("rect")
+      .attr("x", (item, i) => xAxisScale(dates[i]))
+      .attr("y", (item) => height - item)
+      .attr("width", barWidth)
+      .attr("height", (item) => item)
+      .attr("transform", "translate(60, 10)")
+      .attr("fill", "blue")
 });
